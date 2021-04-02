35d65c524a6684c816206b994cca4adcd0a3e028
Dmitry Golubenkov <xxdimonxx15@yandex.ru>
2021-04-02 12:51:23 +0300

    Добавьте файлы проекта.

---
+++ SortTheBallsGameVariant9/AboutApp.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SortTheBallsGameVariant9
+{
+    /// <summary>
+    /// Логика взаимодействия для AboutApp.xaml
+    /// </summary>
+    public partial class AboutApp : Window
+    {
+        public AboutApp()
+        {
+            InitializeComponent();
+        }
+    }
+}
